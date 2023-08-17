df70761f3d49ea9b789b2be44806f99b5f465244
b851TYiytNCk <highpassion90@gmail.com>
2023-08-18 01:07:59 +0300

    add update logic, test version

--- wpsw.php
+++ wpsw.php
@@ -14,6 +14,12 @@
 defined( 'ABSPATH' ) || exit;
 
 
+function add_additonal_wc_mime_types($mime_types) {
+    $mime_types['webp'] = 'image/webp';
+    return $mime_types;
+}
+add_filter('woocommerce_rest_allowed_image_mime_types', 'add_additonal_wc_mime_types', 1, 1);
+
 /**
  * Setting up a WP Cron 
  *  */
@@ -38,32 +44,38 @@ function wpsw_sync_worker() {
     for ($retry = 1; $retry <= $max_retries; $retry++) {
 
 		// setting timeout to 20 seconds for the request to be processed
-        $response = wp_safe_remote_get($products_ext);//, array('timeout' => 20));
+        $response = wp_safe_remote_get($products_ext, array('timeout' => 20));
 
         if (is_wp_error($response)) {
         
 			// log error
             error_log('Error retrieving JSON data: ' . $response->get_error_message());
 
+            continue;
             // schedule the next retry
-            $next_retry = time() + (60 * $retry * $retry); // exponential backoff
-            wp_schedule_single_event($next_retry, 'wpsw_sync_event');
-
-            return;
+            // $next_retry = time() + (60 * $retry * $retry); // exponential backoff
+            // wp_schedule_single_event($next_retry, 'wpsw_sync_event');
+            // return;
         }
 
         // convert JSON and proceed to processing it
         $prod_data = json_decode(wp_remote_retrieve_body($response), true);
-		$valid_message = isset($data_json['message']) && $data_json['message'] === 'OK';
+		$valid_message = isset($prod_data['message']) && $prod_data['message'] === 'OK';
 
         if ($valid_message) {
             // Update WooCommerce products based on $prod_data
 
             // If successful, break out of the loop
 
-            $prod_data_body = $prod_data['data'] ?? false;
+            $prod_data_body = $prod_data['data'] ?? array();
 
-            if ($prod_data_body) {
+            if ( is_array($prod_data_body) && count($prod_data_body) ) {
+
+                $product_load = array(
+                    'create' => array(),
+                    'update' => array(),
+                    'delete' => array()
+                );
 
                 foreach ($prod_data_body as $prod_dataitem) {
                     // Check if the product exists based on SKU
@@ -75,7 +87,7 @@ function wpsw_sync_worker() {
                         'name' => $prod_dataitem['name'],
                         'description' => $prod_dataitem['description'],
                         'regular_price' => $prod_dataitem['price'],
-                        'images' => [
+                        'images' => [       
                             [
                                 'src' => $prod_dataitem['picture'],
                             ],
@@ -102,7 +114,6 @@ function wpsw_sync_worker() {
                     echo 'Product ' . $action . ' with SKU: ' . $product['sku'] . '<br>';
                 }
 
-                // echo '</pre>';
             } else {
                 // data body is absent
                 error_log('Error retrieving JSON data: data array is undefined');
@@ -115,7 +126,7 @@ function wpsw_sync_worker() {
         }
     }
 }
-add_action( 'wpsw_sync_event', 'wpsw_sync_worker' );
+//add_action( 'wpsw_sync_event', 'wpsw_sync_worker' );
 
 
 /**
--- wpsw_woo_api.php
+++ wpsw_woo_api.php
@@ -8,8 +8,8 @@ use Automattic\WooCommerce\Client;
 
 $woocommerce = new Client(
     get_site_url(), // Your store URL
-    'ck_fca70f178d9782227dddc2c3080a569a53a616e3', // Your consumer key
-    'cs_d53152c03331f52b410b9d9e79aa84129dd34ba7', // Your consumer secret
+    'ck_58384434b7640f64e659110585ea83139573212a', // Your consumer key
+    'cs_17d276d58c6612071050d4de5888335e9cd0ccca', // Your consumer secret
     [
         'wp_api' => true, // Enable the WP REST API integration
         'version' => 'wc/v3' // WooCommerce WP REST API version
