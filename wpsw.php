96f9ddd41703eff874406211ce4faaeb75d81755
b851TYiytNCk <highpassion90@gmail.com>
2023-08-21 23:13:36 +0300

    add full image load support without name, add correct product selection for creation

--- wpsw.php
+++ wpsw.php
@@ -39,7 +39,15 @@ function wpsw_sync_worker() {
     require_once(__DIR__ . '/wpsw_woo_api.php'); 
 
     $products_ext = 'https://wp.webspark.dev/wp-api/products';
-    $max_retries = 5; // count 20% failed requests   
+    $max_retries = 5; // count 20% failed requests
+
+    function getSKUArr($arr) {
+        return $arr['sku'];
+    }
+
+    function getSKUObj($obj) {
+        return $obj->sku;
+    }
     
     for ($retry = 1; $retry <= $max_retries; $retry++) {
 
@@ -47,15 +55,9 @@ function wpsw_sync_worker() {
         $response = wp_safe_remote_get($products_ext, array('timeout' => 20));
 
         if (is_wp_error($response)) {
-        
 			// log error
             error_log('Error retrieving JSON data: ' . $response->get_error_message());
-
             continue;
-            // schedule the next retry
-            // $next_retry = time() + (60 * $retry * $retry); // exponential backoff
-            // wp_schedule_single_event($next_retry, 'wpsw_sync_event');
-            // return;
         }
 
         // convert JSON and proceed to processing it
@@ -71,57 +73,119 @@ function wpsw_sync_worker() {
 
             if ( is_array($prod_data_body) && count($prod_data_body) ) {
 
-                // $product_load = array(
-                //     'create' => array(),
-                //     'update' => array(),
-                //     'delete' => array()
-                // );
-
-                foreach ($prod_data_body as $prod_dataitem) {
-                    // Check if the product exists based on SKU
-                    $existing_product = $woocommerce->get('products', ['sku' => $prod_dataitem['sku']]);
-                
-                    // Prepare product data
-                    // $product = [
-                    //     'sku' => $prod_dataitem['sku'],
-                    //     'name' => $prod_dataitem['name'],
-                    //     'description' => $prod_dataitem['description'],
-                    //     'regular_price' => $prod_dataitem['price'],
-                    //     'images' => [       
-                    //         [
-                    //             'src' => uploadImagetoMedia($prod_dataitem['picture']),
-                    //         ],
-                    //     ],
-                    //     'stock_quantity' => $prod_dataitem['in_stock']
-                    // ];
-                    echo '<pre>';
-                    var_dump( uploadImagetoMedia($prod_dataitem['picture']) );
-                    echo '</pre>';
-                    return;
-
-                    // Create or update the product based on SKU presence
-                    // if (empty($existing_product)) {
-                        
-                    //     // SKU not found, create a new product
-                    //     $woocommerce->post('products', $product);
-                    //     $action = 'created';
-
-                    // } else {
-                        
-                    //     // SKU found, update the existing product
-                    //     $product['id'] = $existing_product[0]->id;
-                    //     $woocommerce->put('products/' . $product['id'], $product);
-                    //     $action = 'updated';
-
-                    // }
-                
-                    // echo 'Product ' . $action . ' with SKU: ' . $product['sku'] . '<br>';
+                if (count($prod_data_body) > 2000 ) {
+                    error_log('Error retrieving JSON data: data overload');
+                    exit;
                 }
+
+                $all_current = $woocommerce->get('products');
+
+                $newSKU = array_diff(
+                    array_map('getSKUArr', $prod_data_body),
+                    array_map('getSKUObj', $all_current) 
+                );
+
+                $new_products = array_filter($prod_data_body, function($obj) use ($newSKU) {
+                    return in_array($obj['sku'], $newSKU);
+                });
+
+                echo '<pre>';
+                var_dump( $new_products );
+                echo '</pre>';
+
+                $total_new_p = count($new_products);
+
+                // if ( $total_new_p ) {
+                //     $batch_size = 5;
+
+                //     for ($srt_index = 0; $srt_index < $total_new_p; $srt_index += $batch_size) {
+                //         $batch_pr = array_slice($new_products, $srt_index, $batch_size);
+
+                //         $new_pr_to_load = array();
+
+                //         foreach ($batch_pr as $new_pr) {
+                            
+                //             $pr_data = [
+                //                 'sku' => $new_pr['sku'],
+                //                 'name' => $new_pr['name'],
+                //                 'description' => $new_pr['description'],
+                //                 'regular_price' => $new_pr['price'],
+                //                 'images' => [       
+                //                     [
+                //                         'src' => upload_image_to_media($new_pr['picture']),
+                //                     ],
+                //                 ],
+                //                 'stock_quantity' => $new_pr['in_stock']
+                //             ];
+
+                //             $new_pr_to_load["create"][] = $pr_data;
+                //         }
+                        
+
+                //         if (count($new_pr_to_load)) {
+                //             $pr_upload = $woocommerce->post('products/batch', $new_pr_to_load);
+    
+                //             if (is_wp_error($pr_upload)) {
+                //                 error_log('WP Error: ' . $response->get_error_message());
+                //                 break;
+                //             } else {
+                //                 var_dump( $pr_upload );
+                //             }
+                //         }
+
+                //     }
+
+                // }
+
+                // echo '<pre>';
+                // var_dump( $new_products );
+                // echo '</pre>';
+            
+
+                // foreach ($prod_data_body as $prod_dataitem) {
+                //     // Check if the product exists based on SKU
+                //     $existing_product = $woocommerce->get('products', ['sku' => $prod_dataitem['sku']]);
+                
+                //     // Prepare product data
+                //     $product = [
+                //         'sku' => $prod_dataitem['sku'],
+                //         'name' => $prod_dataitem['name'],
+                //         'description' => $prod_dataitem['description'],
+                //         'regular_price' => $prod_dataitem['price'],
+                //         'images' => [       
+                //             [
+                //                 'src' => upload_image_to_media($prod_dataitem['picture']),
+                //             ],
+                //         ],
+                //         'stock_quantity' => $prod_dataitem['in_stock']
+                //     ];
+                //     // echo '<pre>';
+                //     // var_dump( upload_image_to_media($prod_dataitem['picture']) );
+                //     // echo '</pre>';
+
+                //     // Create or update the product based on SKU presence
+                //     if (empty($existing_product)) {
+                        
+                //         // SKU not found, create a new product
+                //         $woocommerce->post('products', $product);
+                //         $action = 'created';
+
+                //     } else {
+                        
+                //         // SKU found, update the existing product
+                //         $product['id'] = $existing_product[0]->id;
+                //         $woocommerce->put('products/' . $product['id'], $product);
+                //         $action = 'updated';
+
+                //     }
+                
+                //     echo 'Product ' . $action . ' with SKU: ' . $product['sku'] . '<br>';
+                // }
 
             } else {
                 // data body is absent
-                error_log('Error retrieving JSON data: data array is undefined');
-                return;
+                error_log('Error retrieving JSON data: data is incorrect');
+                exit;
             }
 
             break;
@@ -134,41 +198,23 @@ require_once ABSPATH . 'wp-admin/includes/media.php';
 require_once ABSPATH . 'wp-admin/includes/file.php';
 require_once ABSPATH . 'wp-admin/includes/image.php';
 
-function uploadImagetoMedia($src) {
+function upload_image_to_media($src) {
 
-    // $image_data = base64_encode(file_get_contents($src)); //get image content
+    // get protected response obj with proper url of destination source
+    $res = wp_safe_remote_get($src)['http_response'];
 
-    // $response = wp_remote_post(get_site_url() . '/wp-json/wp/v2/media', array(
-    //     'headers'     => array(
-    //         'Authorization' => 'Bearer mZSJxnrC2XIMX65esJ5VuYq7',
-    //         'Content-Type' => 'application/json',
-    //     ),
-    //     'body'        => json_encode(array(
-    //         'file' => $image_data
-    //     )),
-    // ));
+    // Create a reflection class to set response property accessible
+    $reflFirst = new ReflectionClass($res);
+    $reflRes = $reflFirst->getProperty('response');
+    $reflRes->setAccessible(true);
+
+    // Create another reflection to get 'url' property inside 'response' property
+    $reflInner = new ReflectionClass($reflRes->getValue($res));
     
-    // if (is_wp_error($response)) {
-    //     // if upload failed
-    //     $error_message = $response->get_error_message();
-    //     echo "Error uploading image: $error_message";
-    // } else {
-    //     $response_data = json_decode(wp_remote_retrieve_body($response), true);
-    //     echo 'Image uploaded:', print_r($response_data, true);
-    // }
+    $url_to_download = $reflRes->getValue($res)->url;
+    $image_data = media_sideload_image( $url_to_download, 0, '', 'id' ); //get image content
 
-    
-
-        $res = wp_safe_remote_get($src)['http_response'];//media_sideload_image( $src, 0, '', 'id' );
-
-        $reflFirst = new ReflectionClass($res);
-        $reflRes = $reflFirst->getProperty('response');
-        $reflRes->setAccessible(true);
-
-        $reflInner = new ReflectionClass($reflRes->getValue($res));
-        
-
-    return $reflRes->getValue($res)->url;
+    return $image_data;
 
 }
 
